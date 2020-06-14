d415b6dd2759f985403f552269f0083b350b6bbf
HasuNoHana <37709990+HasuNoHana@users.noreply.github.com>
2020-06-14 19:44:27 +0300

    Add files via upload

---
+++ CheckersControllerTest.java
@@ -0,0 +1,14 @@
+package checkers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CheckersControllerTest {
+
+    @Test
+    public void makeMove() {
+        CheckersController controller = new CheckersController(new GameModel(), new GameView());
+
+    }
+}
